f28b24c21edd7d4061a3c07a2196e88db07b1c1c
victor <est.vmedranoz0301@uml.edu.ni>
2024-05-25 01:03:46 -0600

    los 20

---
+++ 01/operaciones_aritmeticas.js
@@ -0,0 +1,16 @@
+// Solicitar al usuario que ingrese el año
+
+let year = parseInt(prompt("Ingresa un año:"));
+
+// Verificar si el año es bisiesto
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+
+  console.log(year + " es un año bisiesto.");
+
+}
+ else {
+
+  console.log(year + " no es un año bisiesto.");
+
+}
---
+++ 02/Comvertir_grados_celsius.js
@@ -0,0 +1,11 @@
+function CelsiusToFarenheit(celsius ){
+
+    var fahrenheit = (celsius * 9/5) + 32;
+    return  fahrenheit ;
+    
+}
+  var Celsius = 25;
+
+  var fahrenheit = celsiusTofahrenheit(celsius) ;
+
+  console.log(celsius + " grados celsius son " + fahrenheit + " grados fahrenheit. ");
